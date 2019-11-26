d78adcf233a10bb2d1d934193b79ebd7b69dbe11
Jan's Laptop <Jan's Laptop@MSI>
2019-11-26 18:23:23 +0100

    test um alles zu commiten

---
+++ SE1_4Way4/OOA_OOD/test/info_OOA_OOD.java
@@ -0,0 +1,9 @@
+package test;
+
+public class info_OOA_OOD {
+
+	
+	/*
+	 * Die Datei ist da um den Pfad in GitHub zu initialisieren
+	 */
+}
---
+++ SE1_4Way4/Tester/test/info_Tester.java
@@ -0,0 +1,9 @@
+package test;
+
+public class info_Tester {
+
+	
+	/*
+	 * Die Datei ist da um den Pfad in GitHub zu initialisieren
+	 */
+}
