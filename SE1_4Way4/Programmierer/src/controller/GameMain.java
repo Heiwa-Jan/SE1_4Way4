d037dde4a1d8c7a408a32f4bc22feaf6661562f3
Christina Jöst <christina_joest@gmx.de>
2019-12-17 23:10:47 +0100

    Added win detection
    added: - SearchRow
    
    updated: - GameMain: - implemented check for winner and return to menu
    					   after game
    
    - Player in SearchRow used to store Player1 as reference for
    identification of winner
    - updated list of method signatures (GameMain and SearchRow)

--- SE1_4Way4/Programmierer/src/controller/GameMain.java
+++ SE1_4Way4/Programmierer/src/controller/GameMain.java
@@ -7,78 +7,124 @@ import src.model.Menu;
 import src.model.Move;
 import src.model.MoveAllTokens;
 import src.model.Player;
+import src.model.SearchRow;
 import src.view.PrintCanvas;
-
-
-
 
 public class GameMain {
 
+	private static boolean running = true;
+
 	public static void main(String[] args) {
-		
+
 		Menu menu = new Menu();
 		GameBoard board;
 		Player player1;
 		Player player2;
 		Move move;
+		SearchRow searchWin;
 		Scanner input = new Scanner(System.in);
-		
-		menu.menuStart();
-		player1 = new Player(menu.getPlayername1());
-		player2 = new Player(menu.getPlayername2());
-		if(menu.isPlayer1Begins()) {
-			player1.setToken('X');
-			player2.setToken('O');
-		}
-		else {
-			player1.setToken('O');
-			player2.setToken('X');
-		}
-		
-		board = new GameBoard(menu.getHeight(), menu.getWidth());
-		while(true) { // später: isRunning()
-			
+
+		while(true) { //ist das Spiel zu Ende, Rückkehr ins Menü
+
+			menu.menuStart();	//das Menü ausführen
+			player1 = new Player(menu.getPlayername1());
+			player2 = new Player(menu.getPlayername2());
 			if(menu.isPlayer1Begins()) {
-				PrintCanvas.print(board.printBoard());
-				PrintCanvas.print(player1.getName()+" gib die Koordinaten einer Position ein");
-				PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
-				PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
-				
-				move = new Move(board);
-				board = MoveAllTokens.move(board, move.getValidMove(), player1.getToken());
-				
-				PrintCanvas.print(board.printBoard());
-				PrintCanvas.print(player2.getName()+" gib die Koordinaten einer Position ein");
-				PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
-				PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
-				
-				move = new Move(board);
-				board = MoveAllTokens.move(board, move.getValidMove(), player2.getToken());
-				
+				player1.setToken('X');
+				player2.setToken('O');
 			}
 			else {
-				PrintCanvas.print(board.printBoard());
-				PrintCanvas.print(player2.getName()+" gib die Koordinaten einer Position ein");
-				PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
-				PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
-				
-				move = new Move(board);
-				board = MoveAllTokens.move(board, move.getValidMove(), player2.getToken());
-				
-				PrintCanvas.print(board.printBoard());
-				PrintCanvas.print(player1.getName()+" gib die Koordinaten einer Position ein");
-				PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
-				PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
-				
-				move = new Move(board);
-				board = MoveAllTokens.move(board, move.getValidMove(), player1.getToken());
-				
-			}		
+				player1.setToken('O');
+				player2.setToken('X');
+			}
+
+			board = new GameBoard(menu.getHeight(), menu.getWidth());
+			searchWin = new SearchRow(board, player1);
+
+			while(isRunning()) { //während niemand gewonnen hat weiterspielen
+
+				if(menu.isPlayer1Begins()) {
+					//1. Zug
+					PrintCanvas.print(board.printBoard()+ "\n ");
+					PrintCanvas.print(player1.getName()+" gib die Koordinaten einer Position ein");
+					PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
+					PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
+
+					move = new Move(board);
+					board = MoveAllTokens.move(board, move.getValidMove(), player1.getToken());
+
+					searchWin.setCurrentBoard(board);
+					setRunning(!searchWin.search());
+
+					if(!isRunning()) {
+						break;
+					}
+					
+					//nächster Zug
+					PrintCanvas.print(board.printBoard() + "\n");
+					PrintCanvas.print(player2.getName()+" gib die Koordinaten einer Position ein");
+					PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
+					PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
+
+					move = new Move(board);
+					board = MoveAllTokens.move(board, move.getValidMove(), player2.getToken());
+
+					searchWin.setCurrentBoard(board);
+					setRunning(!searchWin.search());
+
+				}
+				else {
+					//1. Zug
+					PrintCanvas.print(board.printBoard() + "\n");
+					PrintCanvas.print(player2.getName()+" gib die Koordinaten einer Position ein");
+					PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
+					PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
+
+					move = new Move(board);
+					board = MoveAllTokens.move(board, move.getValidMove(), player2.getToken());
+
+					searchWin.setCurrentBoard(board);
+					setRunning(!searchWin.search());
+
+					if(!isRunning()) {
+						break;
+					}
+
+					//nächster Zug
+					PrintCanvas.print(board.printBoard() + "\n");
+					PrintCanvas.print(player1.getName()+" gib die Koordinaten einer Position ein");
+					PrintCanvas.print("(z.B.: 'a1d' oder '1ad' oder 'a2' oder '2a')");
+					PrintCanvas.print("(r = rechts, l = links, u = oben, d = unten)");
+
+					move = new Move(board);
+					board = MoveAllTokens.move(board, move.getValidMove(), player1.getToken());
+
+					searchWin.setCurrentBoard(board);
+					setRunning(!searchWin.search());
+
+				}	
+
+			}
+
+			//Ausgabe des Endstands und des Gewinners
+			PrintCanvas.print(board.printBoard() + "\n");
+			if(searchWin.isPlayer1Won()) {
+				PrintCanvas.print(player1.getName() + " gewinnt!\n");
+			}
+			else {
+				PrintCanvas.print(player2.getName() + " gewinnt!\n");
+			}
 		}
-		//input.close();		//erst nutzbar, wenn keine Endlosschleife mehr vorhanden!
-								//ACHTUNG: sobald einmal geschlossen, kein weiterer Userinput möglich, bis das gesamte Programm neugestartet wird
+
+	}
 	
-		//die gesamte Eingabe auslagern in separate Klasse in package "View"?
 	
+	//Getter und Setter
+	public static boolean isRunning() {
+		return running;
+	}
+
+	private static void setRunning(boolean running) {
+		GameMain.running = running;
 	}
 }
---
+++ SE1_4Way4/Programmierer/src/model/SearchRow.java
@@ -0,0 +1,89 @@
+package src.model;
+
+public class SearchRow {
+	private GameBoard currentBoard;
+	private boolean player1Won = false;	
+	private Player player1;
+
+	public SearchRow(GameBoard currentboard, Player player){
+		this.setCurrentBoard(currentboard);
+		this.setPlayer1(player);
+	}
+
+	/**	searches for a line of 4 tokens
+	 * 
+	 * @return true if 4-in-one-line is found
+	 */
+	public boolean search() {
+
+		for(int i = 0; i < currentBoard.getRows()-3; i++) {		//Ränder der Arrays dürfen nicht überschritten werden!
+			for(int j = 0; j < currentBoard.getColumns()-3; j++) {
+
+				//wenn Spieler 1 vier in einer Reihe hat, true setzen ( false = Spieler 2 gewinnt)
+				if(player1.getToken() == 'X') {
+					if((currentBoard.getField(j, i) == 'X' ) & (currentBoard.getField(j+1,i) == 'X') & (currentBoard.getField(j+2,i) == 'X') & (currentBoard.getField(j+3,i) == 'X')
+							|| ((currentBoard.getField(i, j) == 'X' ) & (currentBoard.getField(i,j+1) == 'X') & (currentBoard.getField(i,j+2) == 'X') & (currentBoard.getField(i,j+3) == 'X'))
+							|| ((currentBoard.getField(j, j) == 'X' ) & (currentBoard.getField(j+1,j+1) == 'X') & (currentBoard.getField(j+2,j+2) == 'X') & (currentBoard.getField(j+3,j+3) == 'X'))) {
+
+						this.setPlayer1Won(true);
+						return true;
+					}
+
+					if((currentBoard.getField(j, i) == 'O' ) & (currentBoard.getField(j+1,i) == 'O') & (currentBoard.getField(j+2,i) == 'O') & (currentBoard.getField(j+3,i) == 'O')
+							|| 	((currentBoard.getField(i, j) == 'O' ) & (currentBoard.getField(i,j+1) == 'O') & (currentBoard.getField(i,j+2) == 'O') & (currentBoard.getField(i,j+3) == 'O'))
+							|| ((currentBoard.getField(j, j) == 'O' ) & (currentBoard.getField(j+1,j+1) == 'O') & (currentBoard.getField(j+2,j+2) == 'O') & (currentBoard.getField(j+3,j+3) == 'O'))) {
+
+						return true;
+					}
+				}
+
+	
+				if(player1.getToken() == 'O') {
+					if((currentBoard.getField(j, i) == 'X' ) & (currentBoard.getField(j+1,i) == 'X') & (currentBoard.getField(j+2,i) == 'X') & (currentBoard.getField(j+3,i) == 'X')
+							|| ((currentBoard.getField(i, j) == 'X' ) & (currentBoard.getField(i,j+1) == 'X') & (currentBoard.getField(i,j+2) == 'X') & (currentBoard.getField(i,j+3) == 'X'))
+							|| ((currentBoard.getField(j, j) == 'X' ) & (currentBoard.getField(j+1,j+1) == 'X') & (currentBoard.getField(j+2,j+2) == 'X') & (currentBoard.getField(j+3,j+3) == 'X'))) {
+
+
+						return true;
+					}
+			
+					if((currentBoard.getField(j, i) == 'O' ) & (currentBoard.getField(j+1,i) == 'O') & (currentBoard.getField(j+2,i) == 'O') & (currentBoard.getField(j+3,i) == 'O')
+							|| 	((currentBoard.getField(i, j) == 'O' ) & (currentBoard.getField(i,j+1) == 'O') & (currentBoard.getField(i,j+2) == 'O') & (currentBoard.getField(i,j+3) == 'O'))
+							|| ((currentBoard.getField(j, j) == 'O' ) & (currentBoard.getField(j+1,j+1) == 'O') & (currentBoard.getField(j+2,j+2) == 'O') & (currentBoard.getField(j+3,j+3) == 'O'))) {
+
+						this.setPlayer1Won(true);
+						return true;
+					}	
+
+				}
+			}
+		}
+		return false;
+	}
+
+	// Getter und Setter:
+	
+	public GameBoard getCurrentBoard() {
+		return currentBoard;
+	}
+
+	public void setCurrentBoard(GameBoard currentBoard) {
+		this.currentBoard = currentBoard;
+	}
+
+	public Player getPlayer1() {
+		return player1;
+	}
+
+	private void setPlayer1(Player player) {
+		this.player1 = player;
+	}
+
+	public boolean isPlayer1Won() {
+		return player1Won;
+	}
+
+	private void setPlayer1Won(boolean player1Won) {
+		this.player1Won = player1Won;
+	}
+}
