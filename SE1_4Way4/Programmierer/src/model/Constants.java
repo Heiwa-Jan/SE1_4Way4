353125b00128ed308fe0c4f6d84bad80fef6fb81
L0ren2 <57962032+L0ren2@users.noreply.github.com>
2019-12-18 15:09:20 +0100

    Updated Tests and Comments
    SearchRowTests now works for vertical and horzontal, diagonal still
    needs to be done
    
    Constants: just updated a comment

--- SE1_4Way4/Programmierer/src/model/Constants.java
+++ SE1_4Way4/Programmierer/src/model/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 	//Umwandlung der Eingabe in Koordinaten in einem Move-Objekt
 	public static final String coordinatesColumns = "abcdefghij"; 
 	public static final String coordinatesRows = "1234567890"; 
-	public static final String directions = "lrud";
+	public static final String directions = "lrud";//0123
 }
--- SE1_4Way4/Tester/test/firsts/SearchRow1stTest.java
+++ SE1_4Way4/Tester/test/firsts/SearchRow1stTest.java
@@ -1,40 +1,38 @@
 package test.firsts;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
+import src.model.GameBoard;
+import src.model.Move;
+import src.model.MoveAllTokens;
+import src.model.Player;
+import src.model.SearchRow;
+
+
 class SearchRow1stTest {
-	
-/******************************************
-******************************************* 
-**                                       **
-**          ACHTUNG PROTOTYP             **
-**                                       **
-*******************************************
-*******************************************/
-	
-	
+		
 	@Test
 	void winnerTest() {
-		GameBoard testBoard = new GameBoard();
-		testBoard.calculateNewBoard();
-		MoveAllTokens m = new MoveAllTokens();
-		
-//		@ToDo wie konstruiere ich einen Player? was ist mit token X / O
-		Player testPlayer = new Player(1);
-		
-		for(int i = 0; i < 4; i++)
-			m.move(testBoard, new Move("1b", testBoard, testPlayer));
-		
+		GameBoard testBoard = new GameBoard(7, 7);
+		Move newMove = new Move(0, 0, 3, testBoard);//a1u (von oben einwerfen)
+		testBoard = MoveAllTokens.move(testBoard, newMove, 'X');
+
+		Player testPlayer = new Player("Player_1");
+		testPlayer.setToken('X');
+
+		for(int i = 0; i < 4; i++) testBoard = MoveAllTokens.move(testBoard, new Move(1, 2, 3, testBoard), 'X');//1bu
+
+		//Test auf vertikal
 		assertTrue(new SearchRow(testBoard, testPlayer).search());
 
-		testBoard.calculateNewBoard();
-		
-		for(int i = 0; i < 4; i++)
-			m.move(testBoard, new Move("a4", testBoard, testPlayer));
-		
+		testBoard = new GameBoard(7, 7);
+		for(int i = 0; i < 4; i++) testBoard = MoveAllTokens.move(testBoard, new Move(2, 1, 0, testBoard), 'X');//2al
+
+		//Test auf horizontal
 		assertTrue(new SearchRow(testBoard, testPlayer).search());
+		
+		//TODO Test diagonal
 	}
 	
 }
